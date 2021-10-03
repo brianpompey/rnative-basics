1600ffac03801bda5d5acaf4c289f7080a6b087d
brianpompey <brianthescholar@gmail.com>
2021-10-03 18:11:01 -0400

    ADded methofs

--- components/count.js
+++ components/count.js
@@ -3,13 +3,42 @@ import { StyleSheet, Text, View } from 'react-native';
 import Button from "./button";
 
 
+const styles = StyleSheet.create({
+    counterText: {
+      fontSize: 30
+    }
+});
+
+
 class Count extends Component {
     constructor () {
         super();
         this.state = {
             count: 0
-        }
+        };
+        this.upCount = this.upCount.bind(this);
+        this.downCount = this.downCount.bind(this);
+        this.resetCount = this.resetCount.bind(this);
     }
+
+    upCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    downCount = () => {
+        this.setState({
+            count: this.state.count - 1
+        });
+    };
+
+    resetCount = () => {
+        this.setState({
+            count: 0
+        });
+    };
+
 
     render() {
         return(
@@ -17,18 +46,15 @@ class Count extends Component {
                 <Text style={styles.counterText}>
                     {this.state.count}
                 </Text>
-                <Button buttonName="Up"/>
+                <Button buttonName="Up" methodName={this.upCount}/>
                 <Button buttonName="Down"/>
                 <Button buttonName="Reset"/>
             </View>
         )
     }
-}
+};
 
-const styles = StyleSheet.create({
-    counterText: {
-      fontSize: 30
-    }
-});
+
 
 export default Count;
+
