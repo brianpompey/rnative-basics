393d60c5348a529ace5fcdd8c799a50f71cd993a
brianpompey <brianthescholar@gmail.com>
2021-10-03 17:57:48 -0400

    Adding count

---
+++ components/count.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class Count extends Component {
+    constructor () {
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+
+    render() {
+
+    }
+}
+
+export default Count;
